5865e5efc7cd8cff00457fa030af7b6e0c8ffc54
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2013-09-26 01:08:44 +0400

    rubocop changes

--- lib/ftools/runner.rb
+++ lib/ftools/runner.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby -w -U
 # encoding: UTF-8
-# (с) ANB Andrew Bizyaev Андрей Бизяев 
+# (c) ANB Andrew Bizyaev
 
 require_relative 'ruby_version.rb'
 require 'docopt'
@@ -11,41 +11,43 @@ require_relative 'error.rb'
 require_relative 'file_type.rb'
 require_relative 'file_name.rb'
 
+# Foto Tools
 module FTools
 
-  VERSION_CORE = '0.1.1'
+  VERSION_CORE = '0.1.2'
 
   # Main class processing input stream
   class Runner
 
-    def initialize( usage, file_type=[] )
+    def initialize(usage, file_type = [])
       case FTools.os
-      when :windows 
-        ARGV.map!{|a| a.encode("internal", "filesystem")}   #workaround for win32
+      when :windows
+        # workaround for win32
+        ARGV.map! { |a| a.encode('internal', 'filesystem') }
         @os = OSWin.new
-      else 
+      else
         @os = OSUnix.new
-      end  
-      @options_cli = Docopt::docopt( usage, version: "#{FTools::VERSION} (core #{FTools::VERSION_CORE})" )
+      end
+      @options_cli = Docopt.docopt(usage, version:
+                   "#{FTools::VERSION} (core #{FTools::VERSION_CORE})")
       @file_type = file_type
-      FTools::debug = true if @options_cli['--debug']
-      FTools::puts_error "OPTIONS = #{@options_cli.to_s}" if FTools::debug
+      FTools.debug = true if @options_cli['--debug']
+      FTools.puts_error "OPTIONS = #{@options_cli.to_s}" if FTools.debug
       validate_options
 
     rescue Docopt::Exit => e
       STDERR.puts e.message
       exit 1
-    rescue Exception => e
-      FTools::puts_error "FATAL: #{e.message}", e
+    rescue => e
+      FTools.puts_error "FATAL: #{e.message}", e
       exit 1
     end
 
     def run
-      # raise FTools::Error.new("expecting input via pipe (e.g. ftls|#{File.basename($PROGRAM_NAME, ".rb")})") if STDIN.tty?
       return if STDIN.tty?
-      
+
       ARGV.clear
-      
+
       process_before
 
       ARGF.each_line do |line|
@@ -54,44 +56,45 @@ module FTools
         begin
           # checking file
           unless filename && File.exist?(filename)
-            raise FTools::Error.new("does not exist")
+            fail FTools.Error.new('does not exist')
           end
-          if File.directory?(filename)
-            raise FTools::Error.new("not a file")
-          end        
-          raise FTools::Error.new("no permission to write") unless File.writable?(filename)
-          raise FTools::Error.new("wrong type") unless @file_type.include?(File.extname(filename).downcase.slice(1..-1))
-          
-          result = process_file( filename )
-      
-        rescue FTools::Error => e
-          FTools::puts_error "ERROR: '#{line}' - #{e.message}", e
-        else  
+          fail FTools.Error.new('not a file') if File.directory?(filename)
+          fail FTools.Error.new('no permission to write') unless File
+            .writable?(filename)
+          fail FTools.Error.new('wrong type') unless @file_type.include?(File
+            .extname(filename).downcase.slice(1..-1))
+
+          result = process_file(filename)
+
+        rescue FTools.Error => e
+          FTools.puts_error "ERROR: '#{line}' - #{e.message}", e
+        else
           @os.output result unless result.nil?
-        end  
+        end
       end
 
       process_after
 
-    rescue SignalException => e
-      FTools::puts_error "EXIT on user interrupt Ctrl-C"
+    rescue SignalException
+      FTools.puts_error 'EXIT on user interrupt Ctrl-C'
       exit 1
-    rescue Exception => e
-      FTools::puts_error "FATAL: #{e.message}", e
+    rescue => e
+      FTools.puts_error "FATAL: #{e.message}", e
       exit 1
     end
 
     private
+
     def validate_options
     end
-    
+
     def process_before
     end
 
-    def process_file filename
+    def process_file(filename)
       filename
     end
-    
+
     def process_after
     end
   end
