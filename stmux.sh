0d4f93e4560519dc721b3899475f7103e22cf409
Andrey Bizyaev <andrew.bizyaev@gmail.com>
2014-07-20 21:03:40 +0000

    start tmux script

---
+++ stmux.sh
@@ -0,0 +1,24 @@
+CURRENT_SESSION=${PWD##*/}
+
+# Start up the tmux session with specific name
+tmux new-session -d -s $CURRENT_SESSION
+
+# VIM window
+tmux send-keys 'vim' 'C-m'
+tmux rename-window vim
+
+# RSpec window for running tests
+tmux new-window
+tmux rename-window rspec
+
+# Window for running development server
+#tmux new-window
+#tmux rename-window server
+#tmux send-keys 'mailcatcher' 'C-m'
+#tmux send-keys 'rake server' 'C-m'
+
+# Select the first window
+tmux select-window -t 1
+
+# Attach the tmux session
+tmux attach -t $CURRENT_SESSION
